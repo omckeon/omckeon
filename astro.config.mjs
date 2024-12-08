78176ff51cb694fc7bd27d53d70972a012657266
omckeon <ordmckeon@gmail.com>
2024-12-08 18:05:17 +1100

    update deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import partytown from '@astrojs/partytown';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://omckeon.github.io/',
+  // base: 'my-repo',
   integrations: [
     starlight({
       title: "Thoth Tech",
